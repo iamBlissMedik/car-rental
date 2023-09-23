7325ec7c0ebee339d09514afdfe3957c263174e0
Ellams Bliss <ellamsbliss@gmail.com>
2023-09-24 05:16:44 +1000

    filter options using daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import CarsFilterOption from "@/components/Home/CarsFilterOption";
 import Hero from "@/components/Home/Hero";
+import SearchInput from "@/components/Home/SearchInput";
 
 export default function Home() {
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero />
+      <SearchInput />
+      <CarsFilterOption />
     </div>
   );
 }
---
+++ src/components/Home/CarsFilterOption.tsx
@@ -0,0 +1,37 @@
+const CarsFilterOption = () => {
+  return (
+    <div className="mt-10 flex items-center justify-between">
+      <div>
+        <h2 className="text-[30px] font-bold">Cars Catalog</h2>
+        <h2>Explore our cars you might likes</h2>
+      </div>
+      <div className="flex gap-5">
+        <select
+          className="select select-bordered
+            w-full max-w-xs"
+          // onChange={(e) => orderCarList(e.target.value)}
+        >
+          <option disabled selected>
+            Price
+          </option>
+          <option value={-1}>Min to Max</option>
+          <option value={1}>Max to Min</option>
+        </select>
+        <select
+          className="select
+            select-bordered w-full md:block max-w-xs hidden"
+          // onChange={(e) => setBrand(e.target.value)}
+        >
+          <option disabled selected>
+            Manufactural
+          </option>
+          {/* {brandList &&
+            brandList.map((brand: string, index: number) => (
+              <option key={index}>{brand}</option>
+            ))} */}
+        </select>
+      </div>
+    </div>
+  );
+};
+export default CarsFilterOption;
